868b55dfd9beecbac5156fa5450bca7934e9bfcb
MarkWRuszczycky <mwruszczycky@gmail.com>
2018-06-02 14:06:26 -0500

    Comments and refactors Controller
    Also adds in support for using character keys to scroll the swatches.

--- src/Controller.hs
+++ src/Controller.hs
@@ -17,15 +17,22 @@ import Brick.Main     ( ViewportScroll
 import Graphics.Vty   ( Event (..)
                       , Key (..) )
 
-vpScroll :: ViewportScroll Name
-vpScroll = viewportScroll Swatches
-
 mngEvent :: () -> BrickEvent Name e -> EventM Name ( Next () )
+-- ^Manages events, which are presently just for scrolling around in
+-- the window. Any non-supported event ends the program. The
+-- scrolling always works as expected with the arrow keys. Character
+-- keys can also be used for scrolling; however, right now they are
+-- are based on Dvorak setup that I use. This will be changed later.
 mngEvent _ ( VtyEvent ( EvKey k [] ) ) =
     case k of
-         KUp     -> vScrollBy vpScroll (-1) >> continue ()
-         KDown   -> vScrollBy vpScroll 1    >> continue ()
-         KRight  -> hScrollBy vpScroll 1    >> continue ()
-         KLeft   -> hScrollBy vpScroll (-1) >> continue ()
+         KUp       -> vScrollBy vpScroll (-1) >> continue ()
+         KDown     -> vScrollBy vpScroll 1    >> continue ()
+         KRight    -> hScrollBy vpScroll 1    >> continue ()
+         KLeft     -> hScrollBy vpScroll (-1) >> continue ()
+         KChar 'k' -> vScrollBy vpScroll 1    >> continue ()
+         KChar 'j' -> vScrollBy vpScroll (-1) >> continue ()
+         KChar 'h' -> hScrollBy vpScroll (-1) >> continue ()
+         KChar 't' -> hScrollBy vpScroll 1    >> continue ()
          otherwise -> halt ()
+    where vpScroll = viewportScroll Swatches
 mngEvent _ _ = halt ()

64a3322e8de6baea9b61148d21b4c356b94f8a10
MWRuszczycky <mwruszczycky@gmail.com>
2018-11-12 18:55:16 -0600

    Adds types for the three different types of colors

--- src/Types.hs
+++ src/Types.hs
@@ -1,8 +1,14 @@
 module Types
-    ( Mode  (..)
-    , Name  (..)
-    , RGB   (..)
-    , Setup (..)
+    ( Mode         (..)
+    , Name         (..)
+    , RGB          (..)
+    , RGBIndex     (..)
+    , GreyIndex    (..)
+    , ShadeOfColor (..)
+    , ShadeOfGrey  (..)
+    , Ansi         (..)
+    , Setup        (..)
+    , Colorable    (..)
     ) where
 
 import Numeric ( showHex )
@@ -28,3 +34,79 @@ instance Show RGB where
 data Setup = Setup { mode     :: Mode
                    , terminal :: String
                    }
+
+class Colorable a where
+    toRGB  :: a -> RGB
+    encode :: a -> Int
+
+---------------------------------------------------------------------
+-- RGB-type colors
+
+data ShadeOfColor = CS0 | CS1 | CS2 | CS3 | CS4 | CS5
+                    deriving ( Eq, Ord, Show, Enum )
+
+data RGBIndex = RGBIndex ShadeOfColor ShadeOfColor ShadeOfColor
+                deriving ( Eq, Show )
+
+instance Colorable RGBIndex where
+    encode (RGBIndex r g b) = 16 + 36 * fromEnum r + 6 * fromEnum g + fromEnum b
+    toRGB  (RGBIndex r g b) = RGB (go r) (go g) (go b)
+        where go CS0 = 0
+              go CS1 = 95
+              go x   = 40 + go (pred x)
+
+---------------------------------------------------------------------
+-- Greyscale colors
+
+data ShadeOfGrey = GS0  | GS1  | GS2  | GS3  | GS4  | GS5  | GS6  | GS7  |
+                   GS8  | GS9  | GS10 | GS11 | GS12 | GS13 | GS14 | GS15 |
+                   GS16 | GS17 | GS18 | GS19 | GS20 | GS21 | GS22 | GS23
+                   deriving ( Eq, Ord, Show, Enum )
+
+data GreyIndex = GreyIndex ShadeOfGrey deriving ( Eq, Show )
+
+instance Colorable GreyIndex where
+    encode (GreyIndex x) = 232 + fromEnum x
+    toRGB  (GreyIndex x) = let v = fromEnum x * 10 + 8
+                           in  RGB v v v
+
+---------------------------------------------------------------------
+-- Ansi colors and their bold version
+
+data Ansi= Black | Maroon | Green | Olive  | Navy | Purple  | Teal | Silver |
+           Grey  | Red    | Lime  | Yellow | Blue | Fuchsia | Aqua | White
+           deriving ( Eq, Show, Enum )
+
+instance Colorable Ansi where
+    toRGB Black   = RGB   0   0   0
+    toRGB Maroon  = RGB 128   0   0
+    toRGB Green   = RGB   0 128   0
+    toRGB Olive   = RGB 128 128   0
+    toRGB Navy    = RGB   0   0 128
+    toRGB Purple  = RGB 128   0 128
+    toRGB Teal    = RGB   0 128 128
+    toRGB Silver  = RGB 192 192 192
+    toRGB Grey    = RGB 128 128 128
+    toRGB Red     = RGB 255   0 255
+    toRGB Lime    = RGB   0 255   0
+    toRGB Yellow  = RGB 255 255   0
+    toRGB Blue    = RGB   0   0 255
+    toRGB Fuchsia = RGB 255   0 255
+    toRGB Aqua    = RGB   0 255 255
+    toRGB White   = RGB 255 255 255
+    encode Black   =  0
+    encode Maroon  =  1
+    encode Green   =  2
+    encode Olive   =  3
+    encode Navy    =  4
+    encode Purple  =  5
+    encode Teal    =  6
+    encode Silver  =  7
+    encode Grey    =  8
+    encode Red     =  9
+    encode Lime    = 10
+    encode Yellow  = 11
+    encode Blue    = 12
+    encode Fuchsia = 13
+    encode Aqua    = 14
+    encode White   = 15
